831b13aac76e6cb1a89f3f916322ff5059b1cc4b
Manavvv105 <102499873+Manavvv105@users.noreply.github.com>
2025-03-26 08:39:28 -0400

    Update SyntaxErrorTest.java

--- SyntaxErrorTest.java
+++ SyntaxErrorTest.java
@@ -1,14 +1,24 @@
 public class SyntaxErrorTest {
 
+
+    
     public static void main(String[] args) {
+
+
+        
         System.out.println("Starting program..."  // issing closing parenthesis and semicolon
 
+                           
         int number = 10  // Missing semicolon
+            
 
         if (number > 5)  // Missing opening brace {
             System.out.println("Number is greater than 5");
+        
 
         String text = "Test string";  // Correct line
+
+        
         System.out.println(text)  // Missing semicolon
     }
 }
